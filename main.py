0081b7dec63d5b75e5dffef16372fccf26649268
kirby123-cmd <kirubaselvi40805@gmail.com>
2021-10-09 22:18:30 +0530

    commmit changes

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
+import os
+from pml import app
 from flask import Flask,render_template
+
 
 app=Flask(__name__)
 
 @app.route("/")
 def home():
    return render_template("index.html")
-
+port = int(os.getenv('PORT'))
 if __name__=="__main__":
-  app.run(debug=True)
+  app.run(debug=True,port)
